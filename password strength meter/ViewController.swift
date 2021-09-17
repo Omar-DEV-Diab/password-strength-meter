ff08679f762400df53d89f2751eda1fa89b58092
Omar Diab <Omar@Omars-MacBook-Pro.local>
2021-09-17 16:22:17 +0300

    Create example project.

---
+++ password strength meter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  password strength meter
+//
+//  Created by Omar Diab on 9/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
